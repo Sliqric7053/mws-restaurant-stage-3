910de9daae9fdd29c3329c4c22cab3c260144f1f
Sliqric7053 <richardsit@gmail.com>
2018-08-02 07:37:57 +0200

    feat: add image alt attribute

--- js/main.js
+++ js/main.js
@@ -170,6 +170,7 @@ createRestaurantHTML = restaurant => {
 
   const image = document.createElement("img");
   image.className = "restaurant-img";
+  image.alt = `Picture of ${restaurant.name} restaurant`;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
