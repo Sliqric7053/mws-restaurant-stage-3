958c8d9e45a4f883f6831683ffebea588a86e1b4
Sliqric7053 <richardsit@gmail.com>
2018-10-15 07:53:12 +0200

    chore: Update port number

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,9 +7,10 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8887; // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 8000; // Change this to your server port
+    // return `http://localhost:${port}/data/restaurants.json`;
     // return 'http://localhost:1337/restaurants';
+    return `data/restaurants.json`;
   }
 
   /**
